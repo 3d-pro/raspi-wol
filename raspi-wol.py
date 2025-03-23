1dcc97345c22dd40a6065fda4c59fffb71b3f0a7
Kittipun Khantitrirat <boot191@gmail.com>
2025-03-23 12:51:49 +0700

    Update logging

--- raspi-wol.py
+++ raspi-wol.py
@@ -3,6 +3,7 @@ import time
 import socket
 import struct
 import sys
+import logging
 
 # Configuration
 POWER_SWITCH_PIN = 4
@@ -13,12 +14,16 @@ if len(sys.argv) != 2:
 
 MAC_ADDRESS = sys.argv[1]
 
+# Setup logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 # Setup GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(POWER_SWITCH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(LED_PIN, GPIO.OUT)
 
 def send_wol_packet(mac_address):
+    logging.info(f"Sending WOL packet to MAC address: {mac_address}")
     # Construct the Wake-on-LAN "Magic Packet"
     mac_address = mac_address.replace(':', '')
     data = 'FF' * 6 + mac_address * 16
@@ -32,21 +37,26 @@ def send_wol_packet(mac_address):
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     sock.sendto(send_data, ('<broadcast>', 9))
     sock.close()
+    logging.info("WOL packet sent successfully")
 
 def power_switch_callback(channel):
+    logging.info("Power switch pressed")
     GPIO.output(LED_PIN, GPIO.HIGH)
     send_wol_packet(MAC_ADDRESS)
     time.sleep(1)  # Keep the LED on for 1 second
     GPIO.output(LED_PIN, GPIO.LOW)
+    logging.info("LED turned off")
 
 # Add event detection for the power switch
 GPIO.add_event_detect(POWER_SWITCH_PIN, GPIO.RISING, callback=power_switch_callback, bouncetime=300)
 
 try:
+    logging.info("Script started, waiting for power switch press")
     # Keep the script running
     while True:
         time.sleep(1)
 except KeyboardInterrupt:
-    pass
+    logging.info("Script interrupted by user")
 finally:
     GPIO.cleanup()
+    logging.info("GPIO cleanup done")
